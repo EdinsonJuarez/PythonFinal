8703147406f811de463b698a0becaec7518af913
EdinsonJuarez <83055351+EdinsonJuarez@users.noreply.github.com>
2021-04-23 00:57:48 -0500

    ejercicios modulo1
    ejercicios modulo1

---
+++ modulo1/PRACTICA1DATUX.py
@@ -0,0 +1,134 @@
+
+#EJERCICIO 1
+nombre1=input('Ingrese su nombre')
+print('¡Hola  '  f"{nombre1}" '!')
+
+#PROBLEMA2
+numshows=input('Cuantos shows musicales ha visto en el ultimo año: ')
+if numshows > 3:
+    True
+else:
+    False
+
+#PROBLEMA 3
+numdec1=input('Ingrese un numero con decimales: ')
+nument1=input('Ingrese un numero entero: ')
+var3prob3=float(numdec1) + int(nument1)
+print('El resultado de la suma es ' f"{var3prob3}")
+
+#PROBLEMA 4
+payasos=input('Ingrese el numero de payasos: ')
+muñecas=input('Ingrese el numero de muñecas: ')
+pesototalpaquete=int(payasos)*112 + int(muñecas)*75
+print('El peso total es' f"{pesototalpaquete}" 'g')
+
+#PROBLEMA 5
+
+n=input('ingrese el valor de N: ')
+sumaN=n*(n+1)/2
+print("La suma de los N primeros enteros positivos es: " f"{sumaN}")
+
+
+#Problema 6
+edad =input('Ingrese su edad: ')
+numartic=input('Ingrese los articulos comprados: ')
+if edad>18 and numartic>1 :
+    True
+else:
+    False
+
+#Problema7
+
+num1=input('Ingrese un primer numero: ')
+num2=input('Ingrese un primer numero: ')
+if num1==num2 :
+    print('Inserte numeros diferentes')
+elif:
+    if num1>num2:
+        print(num1)
+    elif:
+        print(num2)
+
+
+#Problema8
+num1=input('Ingrese un primer numero: ')
+num2=input('Ingrese un segundo numero: ')
+if num2==0 :
+    print('error')
+else:    
+    div=num1/num2
+    print(float(div))
+
+#Problema9
+
+
+
+
+#Problema10
+
+año=input('Ingrese el año para determinar si es bisiesto: ')
+
+if año%4==0 and año%100!=0: 
+        print(f"{año}"   'es bisiesto')
+else
+    if año%400==0:
+        print(f'{año}' es bisisesto)
+    else   
+        print('no es bisisesto')
+
+#Problema 11
+edadcliente=input('Ingrese la edad del cliente: ')
+
+if edadcliente>=18:
+    print('Pague S/.10')
+elif edadcliente >=4:
+    print('Pague S/.5')
+else:
+    print('Entre gratis')
+
+
+
+#Problema 12
+
+lista1=input('Ingrese una lista cualquiera: ')
+b=lista1[::-1]
+print(b)
+
+
+#Problema 13
+
+lista2=input('Ingrese una lista cualquiera: ')
+lista2.__repr__(4)
+
+lista2
+
+#Problema 14
+
+#Problema 15
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
